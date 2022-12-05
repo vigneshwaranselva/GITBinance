5c6c52bdeff167dbe646645579d55630ecf90f70
vigneshwaranselva <vignesh19s19@gmail.com>
2022-12-05 18:09:35 +0530

    initial code

---
+++ src/test/java/org/newgit/GITClass.java
@@ -0,0 +1,14 @@
+package org.newgit;
+
+public class GITClass {
+	private void method1() {
+		System.out.println("1");
+	}
+	private void method2() {
+		System.out.println("2");
+	}
+	private void method3() {
+		System.out.println("3");
+	}
+
+}
